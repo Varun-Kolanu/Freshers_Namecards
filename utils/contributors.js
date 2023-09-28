53dd2e8c40e5d26772a975d76d7276592f46b4b1
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-09-28 17:36:19 +0530

    initial commit

---
+++ utils/contributors.js
@@ -0,0 +1,23 @@
+const CONTRIBUTERS = [
+    {
+        handle_name: 'cops_iitbhu',
+        name: "First Card",
+        description: "Hello everyone, this is the first card. Go ahead, add your own!",
+        github: "https://github.com/COPS-IITBHU",
+    },
+    // Start adding your data below
+    {
+        handle_name: 'Varun-Kolanu',
+        name: "Varun Kolanu",
+        description: "Hello everyone, myself Varun from CSE Part 2",
+        github: "https://github.com/Varun-Kolanu"
+    },
+    {
+        handle_name: 'darshi1337',
+        name: "Priyadarshi Anupam",
+        description: "Hello everyone, myself Priyadarshi from Mechanical Part 2",
+        github: "https://github.com/darshi1337"
+    },
+];
+
+export default CONTRIBUTERS;
