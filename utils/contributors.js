940471068e17c5285afdca93cbd580936a2a87cc
yusakukudo <anshraj.05.dhakad@gmail.com>
2023-10-01 18:18:09 +0530

    Anshraj commit

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'yusakukudo',
+        name: "Anshraj Dhakad",
+        description: "Hello everyone, myself Anshraj from MnC Part 1",
+        github: "https://github.com/yusakukudo"
+    },
     
 ];
 
