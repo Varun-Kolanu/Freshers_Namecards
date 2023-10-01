b71bb1e08e975e372fa9f1135394dfa8f96fea48
Dhananjay Sahu <146535833+D-jay1@users.noreply.github.com>
2023-10-01 18:46:11 +0530

    Update contributors.js
    added my name

--- utils/contributors.js
+++ utils/contributors.js
@@ -30,12 +30,6 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
-     {
-        handle_name: 'ayushman-05',
-        name: "Ayushman Mishra",
-        description: "Hello everyone, myself Ayushman from EEE Part 1",
-        github: "https://github.com/ayushman-05"
-    },
     { 
     handle_name: 'neharika-garg-20',
     name: "Neharika Garg",
@@ -43,17 +37,6 @@ const CONTRIBUTERS = [
     github:  "https://github.com/neharika-garg-20"},
     
     {
-
-
-
-        handle_name: 'ALPH4quantum',
-        name: "Punit Makwana",
-        description: "Hello everyone, myself Punit from Mech Part 1",
-        github: "https://github.com/ALPH4quantum"
-    },
-
-  {
-
         handle_name: 'priyamvadanegi',
         name: "Priyamvada Negi",
         description: "Hello everyone, this is the Priyamvada Negi. Go ahead, add your own!",
@@ -93,7 +76,6 @@ const CONTRIBUTERS = [
         github: "https://github.com/KaifAsALearner"
     },
     {
-
         handle_name: 'shreyj24',
         name: "Shrey Jaiswal",
         description: "Hello everyone, myself Shrey from Electrial Part 1",
@@ -106,6 +88,13 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Lakshya from MnC Part 1",
         github: "https://github.com/LakshyaKhilwani"
     },
+    {
+
+        handle_name: 'D-jay1',
+        name: "Dhananjay Sahu",
+        description: "Hello everyone, myself Dhananjay from Cera Part 1",
+        github: "https://github.com/D-jay1"
+    },
   {
 
 
@@ -117,7 +106,6 @@ const CONTRIBUTERS = [
         github: "https://github.com/Priyanshu9801"
 
     },
-
 ];
 
 export default CONTRIBUTERS;
