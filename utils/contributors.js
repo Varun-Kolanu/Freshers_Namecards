a018d5bf08ceab431cbcf441899cd83dc79a2c23
GVG-CODER <gaikwad.gauravvilas.cse23@itbhu.ac.in>
2023-10-01 18:27:49 +0530

    hello

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'GVG-CODER',
+        name: "Gaurav Gaikwad",
+        description: "Hello everyone, myself GAURAV from CSE Part 1",
+        github: "https://github.com/GVG-CODER"
+    },
     
 ];
 
