c1bea6c8b5785f8c3429a60b54b9e586138c1053
SartS8528 <sarthaksrivastava4304@gmail.com>
2023-10-01 18:48:10 +0530

    Inital Commit

--- utils/contributors.js
+++ utils/contributors.js
@@ -30,57 +30,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
-<<<<<<< HEAD
     {
         handle_name: 'SartS8528',
         name: "Sarthak Srivastava",
         description: "Hello everyone, myself sarthak from Civil (fresher)",
         github: "https://github.com/SarthakS8528"
     }
-=======
-
-    {
-
-        handle_name: 'sethdivyansh',
-        name: "Divyansh Seth",
-        description: "Hello everyone, I am Divyansh Seth from Pharma Part 1",
-        github: "https://github.com/sethdivyansh"
-    },  
-  {
-        handle_name: 'Krishna9119',
-        name: "Krishna Ishwar Deshmukh",
-        description: "Hello everyone, myself Krishna from MnC Part 1",
-        github: "https://github.com/Krishna9119"
-
-    },
-  {
-
-        handle_name: 'KaifAsALearner',
-        name: "Sk Md Kaif",
-        description: "Hello everyone, myself Kaif from EEE Part 1",
-        github: "https://github.com/KaifAsALearner"
-    },
-    {
-        handle_name: 'shreyj24',
-        name: "Shrey Jaiswal",
-        description: "Hello everyone, myself Shrey from Electrial Part 1",
-        github: "https://github.com/shreyj24"
-    },
-    {
-
-        handle_name: 'LakshyaKhilwani',
-        name: "Lakshya Khilwani",
-        description: "Hello everyone, myself Lakshya from MnC Part 1",
-        github: "https://github.com/LakshyaKhilwani"
-    },
-  {
-
-        handle_name: 'Priyanshu9801',
-        name: "Priyanshu Gupta",
-        description: "Hello everyone, myself Priyanshu from Meta Part 1",
-        github: "https://github.com/Priyanshu9801"
-    },
->>>>>>> dae7ca78a6a7f69b7bbff3ea5372e55c233f27fe
     
 ];
 
