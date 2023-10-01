90c3da694873b81aaac39c4432129eb9702950f0
Rashi Jyotishi <jyotishirashi1610@gmail.com>
2023-10-01 18:19:25 +0530

    added details

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,13 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+
+    {
+        handle_name: 'RashiJyotishi',
+        name: "Rashi Jyotishi",
+        description: "Hello everyone, myself Rashi from Cera Part 1",
+        github: "https://github.com/RashiJyotishi"
+    },
     
 ];
 
