97d013ea4d03dc56ea9561ad8091f580c9aec516
Arnav05laddha <laddhaarnav950@gmail.com>
2023-10-01 18:19:43 +0530

    initial commit

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'Arnav05laddha',
+        name: "Arnav laddha",
+        description: "Hello everyone, myself Arnav from MnC Part 1",
+        github: "https://github.com/Arnav05laddha"
+    },
     
 ];
 
