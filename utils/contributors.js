8d0ab6afc5b786f7a2c14eece0cb3c4ab3f518f3
Priyanshu9801 <priyanshugupta9935@gmail.com>
2023-10-01 18:20:37 +0530

    priyanshu gupta done

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'Priyanshu9801',
+        name: "Priyanshu Gupta",
+        description: "Hello everyone, myself Priyanshu from Meta Part 1",
+        github: "https://github.com/Priyanshu9801"
+    },
     
 ];
 
