4cff4a9283befe465ba77024be632baa1e89cb07
neharika-garg-20 <neharika.garg.cse23@itbhu.ac.in>
2023-10-01 18:16:21 +0530

    initial commit

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,11 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    { 
+        handle_name: 'neharika-garg-20',
+    name: "Neharika Garg",
+    description: "Hello everyone, myself Neharika from CSE Part 1",
+    github:  "https://github.com/neharika-garg-20"},
     
 ];
 
