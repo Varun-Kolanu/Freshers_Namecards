b0631384cba0c7645b783a3e9124b2f43c9213f2
Divyansh Seth <divyanshseth08@gmail.com>
2023-10-01 18:17:39 +0530

    Commit

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'sethdivyansh',
+        name: "Divyansh Seth",
+        description: "Hello everyone, I am Divyansh Seth from Pharma Part 1",
+        github: "https://github.com/sethdivyansh"
+    },  
     
 ];
 
