fa204d3dce3003046275d10e8611b5ef34e66041
ritwik-69 <ritwikpatnaik@gmail.com>
2023-10-01 18:24:07 +0530

    initial commit

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,13 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'ritwik-69',
+        name: "Ritwik Patnaik",
+        description: "Hello everyone, myself Ritwik from civil part 1",
+        github: "https://github.com/ritwik-69"
+    }
+    
     
 ];
 
