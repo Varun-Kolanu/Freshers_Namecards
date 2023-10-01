2b1e99d4c580fd98530285fcf874eeb5333e4925
DrajSkr <sarkardhiraj279@gmail.com>
2023-10-01 18:24:49 +0530

    my fresher namecard

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'DrajSkr',
+        name: "Dhiraj Sarkar  ",
+        description: "Hello everyone, myself Dhiraj from Electral Part 1",
+        github: "https://github.com/DrajSkr"
+    }
     
 ];
 
