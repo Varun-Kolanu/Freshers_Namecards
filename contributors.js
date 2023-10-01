722e95f7e1474fa85398d7adabfdc63a12de17ef
itsvaibhavsingh <146632054+itsvaibhavsingh@users.noreply.github.com>
2023-10-01 18:43:51 +0530

    Add files via upload

---
+++ contributors.js
@@ -0,0 +1,35 @@
+const CONTRIBUTERS = [
+    {
+        handle_name: 'cops_iitbhu',
+        name: "First Card",
+        description: "Hello everyone, this is the first card. Go ahead, add your own!",
+        github: "https://github.com/COPS-IITBHU",
+    },
+    // Start adding your data below
+    {
+        handle_name: 'Varun-Kolanu',
+        name: "Varun Kolanu",
+        description: "Hello everyone, myself Varun from CSE Part 2",
+        github: "https://github.com/Varun-Kolanu"
+    },
+    {
+        handle_name: 'darshi1337',
+        name: "Priyadarshi Anupam",
+        description: "Hello everyone, myself Priyadarshi from Mechanical Part 2",
+        github: "https://github.com/darshi1337"
+    },
+    {
+        handle_name: 'shivansh-bhatnagar18',
+        name: "Shivansh Bhatnagar",
+        description: "Hello everyone, myself Shivansh from MnC Part 2",
+        github: "https://github.com/shivansh-bhatnagar18"
+    },
+    {
+        handle_name: 'itsvaibhavsingh',
+        name: "Vaibhav Singh",
+        description: "Hello everyone, myself Vaibhav Singh from chemical Part 1",
+        github: "https://github.com/itsvaibhavsingh"
+    },
+];
+
+export default CONTRIBUTERS;
