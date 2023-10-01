bff8e405ac8477fa0e29f7dae4fa9cbf8e83b198
shreyj24 <shreyjaiswal3133@gmail.com>
2023-10-01 18:17:34 +0530

    Shrey done

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'shreyj24',
+        name: "Shrey Jaiswal",
+        description: "Hello everyone, myself Shrey from Electrial Part 1",
+        github: "https://github.com/shreyj24"
+    },
     
 ];
 
