e8948384d0dc36ee5212f74d04d5b582568d1ca9
SahilGaikwad2049 <146633518+SahilGaikwad2049@users.noreply.github.com>
2023-10-01 18:46:05 +0530

    Update contributors.js

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+     {
+        handle_name: 'SahilGaikwad2049',
+        name: "Sahil Gaikwad",
+        description: "Hello everyone, myself Sahil Gaikwad from Pharma part 1",
+        github: "https://github.com/SahilGaikwad2049",
+    },
     
 ];
 
