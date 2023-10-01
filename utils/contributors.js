e8fcec55e4cd89d6f0a9d3fb20ca2aa9192f7960
ambarrjain <ambar.jain.cer23@itbhu.ac.in>
2023-10-01 18:20:53 +0530

    tests

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'ambarrjain'
+        name: "Ambar Jain"
+        description : "hi, myself ambar from cera part 1"
+        github: "https://github.com/ambarrjain"
+    }
     
 ];
 
