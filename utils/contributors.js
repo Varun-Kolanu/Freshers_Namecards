d23f044d679d96d8e5af91777131c314902ae533
Anjalihere <itsleelu.peeli@gmail.com>
2023-10-01 18:27:09 -0700

    task done

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'Anjalihere',
+        name: "Anjali Bharti",
+        description: "Hello everyone, myself Anjali Bharti from MnC Part 2",
+        github: "https://github.com/Anjalihere",
+    },
     
 ];
 
